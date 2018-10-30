d423224a12d19e1b6a99914f1e3cd5b224706c18
Shradha Pandey <35809333+ShradhaPandey@users.noreply.github.com>
2018-10-30 13:07:14 +0100

    Update thunderbolt.component.ts

--- src/app/maze/fireball/electroshock/thunderbolt/thunderbolt.component.ts
+++ src/app/maze/fireball/electroshock/thunderbolt/thunderbolt.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NodeInfo } from '../../../../NodeInfo.model';
+import { NodeInfo } from '../../../../nodeProperties.model';
 @Component({
   selector: 'app-thunderbolt',
   templateUrl: './thunderbolt.component.html',
