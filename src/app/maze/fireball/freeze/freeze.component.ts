83bfe3f16afa8798f9ef4c134933e187332f1040
Shradha Pandey <35809333+ShradhaPandey@users.noreply.github.com>
2018-10-30 13:06:33 +0100

    Update freeze.component.ts

--- src/app/maze/fireball/freeze/freeze.component.ts
+++ src/app/maze/fireball/freeze/freeze.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output } from '@angular/core';
-import { NodeInfo } from '../../../NodeInfo.model';
+import { NodeInfo } from '../../../nodeProperties.model';
 @Component({
   selector: 'app-freeze',
   templateUrl: './freeze.component.html',
