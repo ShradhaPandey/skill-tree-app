ff9bd77d977a875ad05342669b5a5fb161a4a939
Shradha Pandey <35809333+ShradhaPandey@users.noreply.github.com>
2018-10-30 13:08:14 +0100

    Update warrior.component.ts

--- src/app/warrior/warrior.component.ts
+++ src/app/warrior/warrior.component.ts
@@ -1,5 +1,5 @@
 import { Component,Output } from '@angular/core';
-import {NodeInfo} from '../NodeInfo.model';
+import {NodeInfo} from '../nodeProperties.model';
 @Component({
   selector: 'app-warrior',
   templateUrl: './warrior.component.html',
